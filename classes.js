dd5c5978339c2958ba9052419f1533a1881aeeb7
madeleine-rashbrooke <madeleine.rashbrooke@gmail.com>
2016-06-26 17:56:06 +1200

    Add colour to circles 2 and 3

--- classes.js
+++ classes.js
@@ -5,7 +5,9 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
+  one()
+  two()
+  three()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -19,7 +21,15 @@ function one () {
 }
 
 // CREATE FUNCTION two HERE
+function two () {
+  var two = document.getElementById('two')
+  two.classList.add('green')
+}
 
 // CREATE FUNCTION three HERE
+function three() {
+  var three = document.getElementById('three')
+  three.classList.add('red')
+}
 
 // CREATE FUNCTION makeVisible HERE
